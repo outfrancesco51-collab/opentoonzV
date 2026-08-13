df9a1f10bf04c6e98d68eaa1fcbc01bd5aa07a9d
outfrancesco51-collab <outfrancesco51@gmail.com>
2026-08-13 19:44:56 +0200

    Fix CMake dependencies for iOS builder: add opencv, glew, superlu, lzo, and let Nixpkgs manage CMAKE_PREFIX_PATH

--- ios-port/flake.nix
+++ ios-port/flake.nix
@@ -30,6 +30,7 @@
       ];
 
       buildInputs = with pkgs; [
+        qt5Env
         qt5.qtbase
         qt5.qtsvg
         qt5.qtmultimedia
@@ -41,6 +42,10 @@
         libjpeg
         libtiff
         zlib
+        opencv4
+        glew
+        superlu
+        lzo
       ];
 
       cmakeFlags = [
@@ -50,22 +55,7 @@
         "-DCMAKE_SYSTEM_NAME=iOS"
         "-DCMAKE_OSX_ARCHITECTURES=arm64"
         "-DCMAKE_SYSTEM_PROCESSOR=aarch64"
-        "-DCMAKE_PREFIX_PATH=${qt5Env}"
         "-DQt5_DIR=${qt5Env}/lib/cmake/Qt5"
-        "-DQt5Core_DIR=${pkgs.qt5.qtbase.dev}/lib/cmake/Qt5Core"
-        "-DQt5Gui_DIR=${pkgs.qt5.qtbase.dev}/lib/cmake/Qt5Gui"
-        "-DQt5Widgets_DIR=${pkgs.qt5.qtbase.dev}/lib/cmake/Qt5Widgets"
-        "-DQt5Network_DIR=${pkgs.qt5.qtbase.dev}/lib/cmake/Qt5Network"
-        "-DQt5OpenGL_DIR=${pkgs.qt5.qtbase.dev}/lib/cmake/Qt5OpenGL"
-        "-DQt5Xml_DIR=${pkgs.qt5.qtbase.dev}/lib/cmake/Qt5Xml"
-        "-DQt5PrintSupport_DIR=${pkgs.qt5.qtbase.dev}/lib/cmake/Qt5PrintSupport"
-        "-DQt5Multimedia_DIR=${pkgs.qt5.qtmultimedia.dev}/lib/cmake/Qt5Multimedia"
-        "-DQt5MultimediaWidgets_DIR=${pkgs.qt5.qtmultimedia.dev}/lib/cmake/Qt5MultimediaWidgets"
-        "-DQt5Svg_DIR=${pkgs.qt5.qtsvg.dev}/lib/cmake/Qt5Svg"
-        "-DQt5Script_DIR=${pkgs.qt5.qtscript.dev}/lib/cmake/Qt5Script"
-        "-DQt5LinguistTools_DIR=${pkgs.qt5.qttools.dev}/lib/cmake/Qt5LinguistTools"
-        "-DQt5SerialPort_DIR=${pkgs.qt5.qtserialport.dev}/lib/cmake/Qt5SerialPort"
-        "-DQt5UiTools_DIR=${pkgs.qt5.qttools.dev}/lib/cmake/Qt5UiTools"
         # OpenToonz-specific flags for mobile/iOS
         "-DWITH_WINTAB=OFF"
         "-DWITH_MIDI=OFF"
